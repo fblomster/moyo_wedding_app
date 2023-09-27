ba7420ce126e9621d0b7e4858efb120401cfcc51
frida <frida.blomster@ha.ax>
2023-09-27 14:24:29 +0300

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,19 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:moyo/provider/locale_provider.dart';
 import 'package:moyo/rsvp_form.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
       ChangeNotifierProvider<LocaleProvider>(
           create: (context) => LocaleProvider(),
-  child: MyApp(),
+  child: const MyApp(),
       )
   );
 }
